5b72363c439c74e95f62eb76f47893d15826ebe7
Nicolas Romero <nromeron@unal.edu.co>
2026-09-06 23:41:51 -0500

    Primeros componentes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer>
+        <div className="footer-content">
+            <small>© 2026 Vite. DevJobs, All rights reserved.</small>
+        </div>
+    </footer>
+  )
+}
+
